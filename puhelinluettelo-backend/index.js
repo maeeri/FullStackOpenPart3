d7e27d170b08148802795c739ce82ccb54d2029d
Mari-Anne Eerikäinen <mari-anne.eerikainen@awacademy.training>
2022-10-05 08:48:13 +0300

    3.13

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -2,6 +2,9 @@ const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
+const { default: mongoose } = require('mongoose')
 
 const app = express()
 
@@ -12,11 +15,14 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
-let persons = [
+let people = []
+
+/*[
     { 
         id: 1,
         name: 'Arto Hellas', 
@@ -40,57 +46,64 @@ let persons = [
 
 const generateId = () => {
     return Math.floor(Math.random() * 25000)
-}
+}*/
 
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    res.send(`<p>Phonebook has info for a number of people</p><p>${date}</p>`)
 })
 
-app.get('/api/persons/', (req, res) => {
-    res.json(persons)
+app.get('/api/people/', (req, res) => {
+    Person.find({}).then(people => {
+        console.log(res)
+        res.json(people)
+    })
 })
-
+  
 app.get('/', (req, res) => {
     res.send('<h1 style="text-align:center">Welcome</h1>')
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+app.get('/api/people/:id', (req, res) => {
+    Person.findById(req.params.id).then(person => {
+        res.json(person)
+    })
+
+    /*const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
     if (person) {
         res.json(person)
     } else {
         res.status(404).end()
-    }
+    }*/
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/people/:id', (req, res) => {
     const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
+    people = people.filter(p => p.id !== id)
     res.status(204).end()
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/people', (req, res) => {
     const body = req.body
-    console.log(body)
+    //console.log(body)
 
-    if (!(body.name || body.number)) {
+    if (body.name === undefined || body.number === undefined) {
         res.status(400).json({
             error: 'not a valid contact'
         })
     }
 
-    const newPerson = {
-        id: generateId(),
+    const person = {
         name: body.name,
         number: body.number
     }
 
 
-    if (persons.every(p => p.name !== newPerson.name )) {
-        persons = persons.concat(newPerson)
-        res.json(newPerson)
+    if (people.every(p => p.name !== person.name )) {
+        person.save().then(sPerson => {
+            res.json(sPerson)
+        })
     } else {
         res.status(400).json({
             error: 'person already in the phonebook'
